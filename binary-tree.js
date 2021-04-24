bd5d785720909c6709d9f026c032edd4b7f20d6c
Julia Ayres <j.bobrineva@gmail.com>
2021-04-24 18:20:28 -0400

    passed tests for nextLarger() in binary-tree.js

--- binary-tree.js
+++ binary-tree.js
@@ -57,7 +57,6 @@ class BinaryTree {
 			total = Math.max(total, node.val + leftSum + rightSum);
 			return Math.max(0, leftSum + node.val, rightSum + node.val);
 		};
-
 		max(this.root);
 		return total;
 	}
@@ -65,39 +64,22 @@ class BinaryTree {
 	/** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-	// nextLarger(lowerBound) {
+	nextLarger (lowerBound) {
+		if (!this.root) return null;
 
-	// }
+		let queue = [ this.root ];
+		let closest = null;
 
-	/** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-	// areCousins(node1, node2) {
-
-	// }
-
-	/** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-	// static serialize() {
-
-	// }
-
-	/** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-	// static deserialize() {
-
-	// }
-
-	/** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-	// lowestCommonAncestor(node1, node2) {
-
-	// }
+		while (queue.length) {
+			let current = queue.shift();
+			if (current.val > lowerBound && (current.val < closest || closest === null)) {
+				closest = current.val;
+			}
+			if (current.left) queue.push(current.left);
+			if (current.right) queue.push(current.right);
+		}
+		return closest;
+	}
 }
 
 module.exports = { BinaryTree, BinaryTreeNode };
